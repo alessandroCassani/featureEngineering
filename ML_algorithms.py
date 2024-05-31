68da47a9e815a391f197d858416389277a429ecb
Luca Perfetti <l.perfetti2001@gmail.com>
2024-05-31 20:59:11 +0200

    systemized training and testing

--- ML_algorithms.py
+++ ML_algorithms.py
@@ -13,19 +13,17 @@ from scipy.stats import randint
 from sklearn.model_selection import learning_curve
 import scipy.stats as st  
 
-def train_decision_tree_model(df, test_df):
-    # Splitting the dataset into features and target variable
-    X = df.drop('stroke', axis=1)
-    y = df['stroke']
-    
-    X_test_df = test_df.drop('stroke', axis=1)
-    y_test_df = test_df['stroke']
-    
-    # Splitting the dataset into the Training set and Test set for test df
-    X_train_df_test, X_test_df_test, y_train_df_test, y_test_df_test = train_test_split(X_test_df, y_test_df, test_size=0.3, random_state=42)
+def train_decision_tree_model(df_dirty, df_original):
+    # Splitting the dataset con duplicati into features and target variable
+    X = df_dirty.drop('stroke', axis=1)
+    y = df_dirty['stroke']
 
-    # Splitting the dataset into the Training set and Test set for training df
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    # Splitting the dataset originale into features and target variable
+    X_test_original = df_original.drop('stroke', axis=1)
+    y_test_original = df_original['stroke']
+
+    # Splitting the dataset into training set and test set (with 30% testing)
+    X_train_dirty, X_test_dirty, y_train_dirty, y_test_dirty = train_test_split(X, y, test_size=0.3, random_state=42)
 
     # Creating the decision tree classifier
     tree_classifier = DecisionTreeClassifier(random_state=42)
@@ -44,7 +42,7 @@ def train_decision_tree_model(df, test_df):
 
     # Measuring the start time for hyperparameter search
     start_time_hyperparameter_search = time()
-    random_search.fit(X_train, y_train)
+    random_search.fit(X_train_dirty, y_train_dirty)
     end_time_hyperparameter_search = time()
     hyperparameter_search_time = end_time_hyperparameter_search - start_time_hyperparameter_search
 
@@ -52,40 +50,40 @@ def train_decision_tree_model(df, test_df):
     best_params = random_search.best_params_
     best_tree_classifier = random_search.best_estimator_
 
-    # Printing the best parameters and their ROC AUC score on the training set
-    y_train_pred_prob = best_tree_classifier.predict_proba(X_train)[:, 1]
-    roc_auc_train = roc_auc_score(y_train, y_train_pred_prob)
-
     # Training the model on the entire training set and measuring training time
     start_time_training = time()
-    best_tree_classifier.fit(X_train, y_train)
+    best_tree_classifier.fit(X_train_dirty, y_train_dirty)
     end_time_training = time()
     dt_training_time = end_time_training - start_time_training
 
-    # Predictions on the training and test sets
-    y_train_pred = best_tree_classifier.predict(X_train_df_test)
-    y_test_pred = best_tree_classifier.predict(X_test_df_test)
+    # Predictions on test sets
+    y_train_pred_dirty = best_tree_classifier.predict(X_train_dirty)
+    y_test_pred_dirty = best_tree_classifier.predict(X_test_dirty)
+    y_test_pred_original = best_tree_classifier.predict(X_test_original)
 
-    # Printing performance on the training set
-    print("\nPrestazioni sul Set di Addestramento:")
-    print(classification_report(y_train, y_train_pred))
+    # Valutazione delle prestazioni sul set di addestramento
+    print("Classification Report on Training Set:")
+    print(classification_report(y_train_dirty, y_train_pred_dirty))
 
-    # Printing performance on the test set
-    print("\nPrestazioni sul Set di Test:")
-    print(classification_report(y_test, y_test_pred))
+    # Printing performance on the test set dirty
+    print("Classification Report on Test Set - dirty:")
+    print(classification_report(y_test_dirty, y_test_pred_dirty))
+
+    # Printing performance on the test set original
+    print("Classification Report on Test Set - original:")
+    print(classification_report(y_test_original, y_test_pred_original))
 
     # Printing the best parameters and time taken for hyperparameter search and training
     print("\nMigliori Parametri:", best_params)
     print("Tempo impiegato per la Ricerca degli Iperparametri:", hyperparameter_search_time, "secondi")
     print("Tempo impiegato per l'Addestramento:", dt_training_time, "secondi")
     
-    plot_decision_tree(random_search.best_estimator_, feature_names=X_train.columns)
-    plot_feature_importance_decision_tree(best_tree_classifier, X)
-    plot_roc_curve(y_test, best_tree_classifier, X_test)
-    plot_confusion_matrix(y_test, y_test_pred)
-    
-    return best_tree_classifier
+    plot_decision_tree(random_search.best_estimator_, feature_names=X_train_dirty.columns)
+    plot_feature_importance_decision_tree(best_tree_classifier, X_train_dirty)
+    plot_roc_curve(y_test_original, best_tree_classifier, X_test_original)
+    plot_confusion_matrix(y_test_original, y_test_pred_original)
 
+    return best_tree_classifier
 
 def plot_decision_tree(tree_model, feature_names, class_names=['0', '1']):
     plt.figure(figsize=(20, 10))
@@ -170,12 +168,17 @@ def k_fold_cross_validation_dt(model, df):
     plt.show()
 
 
-def train_hist_gradient_boosting_model(df):
-    X = df.drop('stroke', axis=1)
-    y = df['stroke']
+def train_hist_gradient_boosting_model(df_dirty, df_original):
+    # Splitting the dataset con duplicati into features and target variable
+    X = df_dirty.drop('stroke', axis=1)
+    y = df_dirty['stroke']
 
-    # Splitting the dataset into the Training set and Test set
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    # Splitting the dataset originale into features and target variable
+    X_test_original = df_original.drop('stroke', axis=1)
+    y_test_original = df_original['stroke']
+
+    # Splitting the dataset into training set and test set (with 30% testing)
+    X_train_dirty, X_test_dirty, y_train_dirty, y_test_dirty = train_test_split(X, y, test_size=0.3, random_state=42)
 
     # Define the parameter grid
     param_grid = {
@@ -201,7 +204,7 @@ def train_hist_gradient_boosting_model(df):
     )
 
     start_time = time()
-    random_search.fit(X_train, y_train)
+    random_search.fit(X_train_dirty, y_train_dirty)
     end_time = time()
     search_time = end_time - start_time
     
@@ -217,22 +220,30 @@ def train_hist_gradient_boosting_model(df):
     best_model = random_search.best_estimator_
 
     # Predictions on training set
-    train_predictions = best_model.predict(X_train)
+    train_predictions = best_model.predict(X_train_dirty)
 
     # Print classification report for training set
-    print("Classification Report on Training Set:")
-    print(classification_report(y_train, train_predictions))
+    print("Classification Report on Training Set - dirty:")
+    print(classification_report(y_train_dirty, train_predictions))
 
-    # Predictions on test set
-    test_predictions = best_model.predict(X_test)
+    # Predictions on test set dirty
+    test_predictions_dirty = best_model.predict(X_test_dirty)
 
     # Print classification report for test set
-    print("Classification Report on Test Set:")
-    print(classification_report(y_test, test_predictions))
+    print("Classification Report on Test Set - dirty:")
+    print(classification_report(y_test_dirty, test_predictions_dirty))
     
-    plot_roc_curve(y_test, best_model, X_test)
-    plot_confusion_matrix(y_test, test_predictions)
-    plot_learning_curve(HistGradientBoostingClassifier(), X_train, y_train, cv=5)
+    # Predictions on test set original
+    test_predictions_original = best_model.predict(X_test_original)
+
+    # Print classification report for test set original
+    print("Classification Report on Test Set - original:")
+    print(classification_report(y_test_original, test_predictions_original))
+    
+
+    plot_roc_curve(y_test_original, best_model, X_test_original)
+    plot_confusion_matrix(y_test_original, test_predictions_original)
+    plot_learning_curve(HistGradientBoostingClassifier(), X_train_dirty, y_train_dirty, cv=5)
 
     return best_model
 

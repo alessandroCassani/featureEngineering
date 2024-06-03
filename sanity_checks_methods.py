56c4652c1303f93cc538a827c04e3a805bcbffab
alessandroCassani <acassani@studenti.uninsubria.it>
2024-06-04 00:12:15 +0200

    fixed bug in inconsistency counter

--- sanity_checks_methods.py
+++ sanity_checks_methods.py
@@ -76,8 +76,8 @@ def check_age_married_consistency(df):
     print(len(invalid_rows_index))
     
 def drop_inconsistencies(df):
-    invalid_rows_index = df[(df['age'] < 16) & (df['ever_married'] == 1)].index
-    df = df.drop(invalid_rows_index, axis=0)
+    invalid_rows_index_age_married = df[(df['age'] < 16) & (df['ever_married'] == 1)].index
+    df = df.drop(invalid_rows_index_age_married, axis=0)
     invalid_rows_index = df[(df['age'] < 16) & ((df['work_type'] != 0) | (df['work_type'] != 1))].index
     df = df.drop(invalid_rows_index, axis=0)
     df = df[df['sex'] >= 0]

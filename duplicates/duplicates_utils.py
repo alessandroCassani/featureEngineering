a45ee805bc8694850c884e18c76fe525fd561a8d
Luca Perfetti <l.perfetti2001@gmail.com>
2024-06-05 23:52:50 +0200

    created method replace_duplicates_values()

--- duplicates/duplicates_utils.py
+++ duplicates/duplicates_utils.py
@@ -24,9 +24,12 @@ def print_duplicates_values(df):
     duplicate_percentage = (duplicate_counts / total_rows) * 100
     print("Percentage of Duplicate Values: ", duplicate_percentage)
 
-def duplicates_values(df, percentage):
+def replace_duplicates_values(df, percentage):
     num_duplicates = int(len(df) * (percentage / 100))
-    indices_to_duplicate = np.random.choice(df.index, size=num_duplicates, replace=True)
-    duplicated_data = df.loc[indices_to_duplicate]
-    df_with_duplicates = pd.concat([df, duplicated_data], ignore_index=True)
+    indices_to_drop = np.random.choice(df.index, size=num_duplicates, replace=False)
+    df_removed = df.drop(indices_to_drop).reset_index(drop=True)
+    indices_to_duplicates = np.random.choice(df_removed.index, size=num_duplicates, replace=True)
+    duplicated_data = df_removed.loc[indices_to_duplicates]
+    df_with_duplicates = pd.concat([df_removed, duplicated_data], ignore_index=True)
+
     return df_with_duplicates

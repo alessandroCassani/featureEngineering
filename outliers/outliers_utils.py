3ae38b98191e76b5dee4e5747bcbe661c37c794a
Luca Perfetti <l.perfetti2001@gmail.com>
2024-06-10 21:51:28 +0200

    create method to add categorical outliers

--- outliers/outliers_utils.py
+++ outliers/outliers_utils.py
@@ -2,7 +2,6 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
-import random
 
 def detect_outliers_zscore(df, feature, threshold):
     z_scores = np.abs((df[feature] - df[feature].mean()) / df[feature].std())
@@ -25,7 +24,7 @@ def visualize_outliers_specific(df, feature):
     else:
         print('no outliers detected')
 
-# maybe it makes delete
+# maybe it makes delete - uniform
 def add_outlier_continuous(df, feature, percentage):
     mean = df[feature].mean()
     std = df[feature].std()
@@ -45,6 +44,7 @@ def add_outlier_continuous(df, feature, percentage):
     df.loc[outlier_indices, feature] = outliers
     return df
 
+# variable normal
 def outliers_replace(df, feature, percentage):
     # Removed rows from the dataset
     num_rows = int(len(df) * (percentage / 100))
@@ -77,3 +77,45 @@ def drop_negative_values(df, feature):
 
 def drop_negative_age(df):
     df[df['age'] >= 0]
+
+def add_categorical_outliers(feature, percentage, df):
+    # Calcola il valore outlier per la colonna specificata
+    outlier_value = df[feature].value_counts().idxmin()
+    print("Valore meno frequente: ", outlier_value)
+
+    # Determina il numero di righe da modificare
+    n_rows = int(len(df) * (percentage / 100))
+
+    # Seleziona casualmente le righe da modificare
+    rows_to_modify = np.random.choice(df.index, size=n_rows, replace=False)
+
+    # Inserisci il valore outlier nella percentuale di righe specificata
+    df.loc[rows_to_modify, feature] = outlier_value
+    return df
+
+def detect_outliers_categorical(df, feature, threshold):
+    # Calcola la frequenza delle categorie
+    value_counts = df[feature].value_counts()
+    total_count = len(df[feature])
+    # Identifica le categorie che sono sotto la soglia (percentuale)
+    outliers = value_counts[value_counts / total_count < threshold].index
+    print(f"Value counts:\n{value_counts}")  # Debug: stampa delle frequenze
+    print(f"Outliers detected: {outliers.tolist()}")  # Debug: stampa degli outlier
+    return outliers
+
+def visualize_outliers_categorical(df, feature):
+    threshold = 3
+    outliers = detect_outliers_categorical(df, feature, threshold)
+    
+    if len(outliers) > 0:
+        print("Outliers found:")
+        plt.figure(figsize=(10, 6))
+        sns.countplot(data=df, x=feature, order=df[feature].value_counts().index)
+        plt.title(f'Count Plot of {feature}')
+        plt.xlabel(feature)
+        plt.ylabel('Frequency')
+        
+        plt.xticks(rotation=45)
+        plt.show()
+    else:
+        print('No outliers detected')

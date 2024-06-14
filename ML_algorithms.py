1a07750cfe133b21e61383ef1a34b2b74f381c5f
Luca Perfetti <l.perfetti2001@gmail.com>
2024-06-15 01:10:58 +0200

    conclusion ok

--- ML_algorithms.py
+++ ML_algorithms.py
@@ -25,7 +25,6 @@ def plot_decision_tree(tree_model, feature_names, class_names=['0', '1']):
     text = tree_plot[0]
     print("Node Labels:\n", text)
     plt.show()
-
     
 def plot_feature_importance_decision_tree(best_tree_classifier, X):
     importance = best_tree_classifier.feature_importances_
@@ -39,29 +38,12 @@ def plot_feature_importance_decision_tree(best_tree_classifier, X):
     plt.ylabel('Feature')
     plt.title('Feature Importance Plot')
     plt.show()
-    
-    
-def plot_roc_curve(y_test, classifier, X_test):
-    y_pred_prob = classifier.predict_proba(X_test)[:, 1]
-    fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
-    roc_auc = roc_auc_score(y_test, y_pred_prob)
-    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic (ROC) Curve')
-    plt.legend(loc="lower right")
-    plt.show()
-    print("AUC Score:", roc_auc)
-    
+        
 def plot_confusion_matrix(y_test, y_test_pred):
     cm = confusion_matrix(y_test, y_test_pred)
     labels = [1, 0]
     disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
     disp.plot()
-    
     
 def k_fold_cross_validation_dt(model, df):
     X = df.drop('stroke', axis=1)
@@ -151,46 +133,17 @@ def model_svm(df_dirty, df_original):
     print(classification_report(y_test_original, y_pred_original))
     
     # Plot ROC curve
-    plot_roc_curve_svm(y_test_original, pipeline, X_test_original)
+    y_pred_prob_svm, y_test_svm = plot_roc_curve_svm(y_test_original, pipeline, X_test_original)
     
     # Plot confusion matrix
     plot_confusion_matrix(y_test_original, y_pred_original)
     
-    return pipeline
+    return y_pred_prob_svm, y_test_svm, pipeline
     
-def model_dt(df_dirty, df_original):
-    # Splitting the dataset con duplicati into features and target variable
-    X_dirty = df_dirty.drop('stroke', axis=1)
-    y_dirty = df_dirty['stroke']
-    
-    X_original = df_original.drop('stroke', axis=1)
-    y_original = df_original['stroke']
-
-    X_train_original, X_test_original, y_train_original, y_test_original = train_test_split(X_original, y_original, test_size=0.3, random_state=42)
-
-    # Splitting the dirty dataset into training set and test set (with 30% testing)
-    X_train_dirty, X_test_dirty, y_train_dirty, y_test_dirty = train_test_split(X_dirty, y_dirty, test_size=0.3, random_state=42)
-    
-    decision_tree_model = DecisionTreeClassifier(max_depth=10, random_state=0)
-    decision_tree_model.fit(X_train_dirty, y_train_dirty)
-    y_pred_original = decision_tree_model.predict(X_test_original)
-    
-
-    # Printing performance on the test set original
-    print("Classification Report on Test Set - original:")
-    print(classification_report(y_test_original, y_pred_original))
-    
-    plot_decision_tree(decision_tree_model,df_original.columns,)
-    plot_feature_importance_decision_tree(decision_tree_model, X_train_dirty)
-    plot_roc_curve(y_test_original, decision_tree_model, X_test_original)
-    plot_confusion_matrix(y_test_original, y_pred_original)
-    return decision_tree_model
-
-
-def plot_roc_curve_svm(y_test, classifier, X_test):
-    y_pred_prob = classifier.decision_function(X_test)
-    fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
-    roc_auc = roc_auc_score(y_test, y_pred_prob)
+def plot_roc_curve_svm(y_test_svm, classifier, X_test):
+    y_pred_prob_svm = classifier.decision_function(X_test)
+    fpr, tpr, thresholds = roc_curve(y_test_svm, y_pred_prob_svm)
+    roc_auc = roc_auc_score(y_test_svm, y_pred_prob_svm)
     plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
     plt.plot([0, 1], [0, 1], 'k--')
     plt.xlim([0.0, 1.0])
@@ -201,9 +154,10 @@ def plot_roc_curve_svm(y_test, classifier, X_test):
     plt.legend(loc="lower right")
     plt.show()
     print("AUC Score:", roc_auc)
+    return y_pred_prob_svm, y_test_svm 
     
 def decision_tree(df):
-    # Splitting the dataset con duplicati into features and target variable
+    # Splitting the dataset con duplicates into features and target variable
     X = df.drop('stroke', axis=1)
     y = df['stroke']
 
@@ -226,9 +180,9 @@ def decision_tree(df):
     
     plot_decision_tree(decision_tree_model,df.columns)
     plot_feature_importance_decision_tree(decision_tree_model, X_train)
-    plot_roc_curve(y_test, decision_tree_model, X_test)
+    y_pred_prob, y_test = plot_roc_curve(y_test, decision_tree_model, X_test)
     plot_confusion_matrix(y_test, pred_test)
-    return decision_tree_model
+    return y_pred_prob, y_test, decision_tree_model
 
 def SVM(df):    
     # Split the original dataset into features and target variable
@@ -256,12 +210,12 @@ def SVM(df):
     print(classification_report(y_test, pred_test))
     
     # Plot ROC curve
-    plot_roc_curve_svm(y_test, svm_model, X_test)
+    y_pred_prob, y_test = plot_roc_curve_svm(y_test, svm_model, X_test)
     
     # Plot confusion matrix
     plot_confusion_matrix(y_test, pred_test)
     
-    return svm_model    
+    return y_pred_prob, y_test, svm_model    
     
 def model_nb(df_dirty,df_original):
     X_dirty = df_dirty.drop('stroke', axis=1)
@@ -292,6 +246,159 @@ def model_nb(df_dirty,df_original):
     y_pred_original = model.predict(X_test_original)
 
     print("Classification Report on Test Set - original:")
+    print(classification_report(y_test_original, y_pred_original))  
+
+def model_dt(df_dirty, df_original):
+    # Splitting the dataset con duplicati into features and target variable
+    X_dirty = df_dirty.drop('stroke', axis=1)
+    y_dirty = df_dirty['stroke']
+    
+    X_original = df_original.drop('stroke', axis=1)
+    y_original = df_original['stroke']
+
+    X_train_original, X_test_original, y_train_original, y_test_original = train_test_split(X_original, y_original, test_size=0.3, random_state=42)
+
+    # Splitting the dirty dataset into training set and test set (with 30% testing)
+    X_train_dirty, X_test_dirty, y_train_dirty, y_test_dirty = train_test_split(X_dirty, y_dirty, test_size=0.3, random_state=42)
+    
+    decision_tree_model = DecisionTreeClassifier(max_depth=10, random_state=0)
+    decision_tree_model.fit(X_train_dirty, y_train_dirty)
+    y_pred_original = decision_tree_model.predict(X_test_original)
+
+    # Printing performance on the test set original
+    print("Classification Report on Test Set - original:")
     print(classification_report(y_test_original, y_pred_original))
     
-    
+    plot_decision_tree(decision_tree_model,df_original.columns,)
+    plot_feature_importance_decision_tree(decision_tree_model, X_train_dirty)
+    y_pred_prob, y_test = plot_roc_curve(y_test_original, decision_tree_model, X_test_original)
+    plot_confusion_matrix(y_test_original, y_pred_original)
+    return y_pred_prob, y_test, decision_tree_model
+
+def plot_roc_curve(y_test, classifier, X_test):
+    y_pred_prob = classifier.predict_proba(X_test)[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
+    roc_auc = roc_auc_score(y_test, y_pred_prob)
+    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.legend(loc="lower right")
+    plt.show()
+    print("AUC Score:", roc_auc)
+    return y_pred_prob, y_test
+
+def plot_roc_curve_conlusion(y_pred_prod_dt, y_test_dt, y_pred_prod_svm, y_test_svm):
+    # Calcolo della curva ROC per ogni modello
+    fpr1, tpr1, thresholds1 = roc_curve(y_test_dt, y_pred_prod_dt)
+    roc_auc1 = roc_auc_score(y_test_dt, y_pred_prod_dt)
+
+    # Calcolo della curva ROC per ogni modello
+    fpr2, tpr2, thresholds2 = roc_curve(y_test_svm, y_pred_prod_svm)
+    roc_auc2 = roc_auc_score(y_test_svm, y_pred_prod_svm)
+
+    # Plot delle curve ROC
+    plt.figure(figsize=(10, 8))
+
+    # Modello 1 - Decision Tree
+    plt.plot(fpr1, tpr1, color='darkorange', lw=2, label='Decision Tree(AUC = %0.2f)' % roc_auc1)
+
+    # Modello 2 - Support Vector Machine
+    plt.plot(fpr2, tpr2, color='blue', lw=2, label='Support Vector Machine(AUC = %0.2f)' % roc_auc2)
+
+    # Linea di riferimento
+    plt.plot([0, 1], [0, 1], 'k--', lw=2)
+
+    # Impostazioni del grafico
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Confronto delle curve ROC')
+    plt.legend(loc="lower right")
+
+    # Mostra il grafico
+    plt.show()
+
+def plot_roc_curve_conlusion_dt(y_pred_prod_Age, y_test_Age, y_pred_prod_glucose, y_test_glucose,
+                                  y_pred_prod_bmi, y_test_bmi, y_pred_prod_categorical, y_test_categorical):
+    # Calcolo della curva ROC per ogni modello
+    fpr1, tpr1, thresholds1 = roc_curve(y_test_Age, y_pred_prod_Age)
+    roc_auc1 = roc_auc_score(y_test_Age, y_pred_prod_Age)
+
+    # Calcolo della curva ROC per ogni modello
+    fpr2, tpr2, thresholds2 = roc_curve(y_test_glucose, y_pred_prod_glucose)
+    roc_auc2 = roc_auc_score(y_test_glucose, y_pred_prod_glucose)
+
+    # Calcolo della curva ROC per ogni modello
+    fpr3, tpr3, thresholds3 = roc_curve(y_test_bmi, y_pred_prod_bmi)
+    roc_auc3 = roc_auc_score(y_test_bmi, y_pred_prod_bmi)
+
+    # Calcolo della curva ROC per ogni modello
+    fpr4, tpr4, thresholds4 = roc_curve(y_test_categorical, y_pred_prod_categorical)
+    roc_auc4 = roc_auc_score(y_test_categorical, y_pred_prod_categorical)
+
+    # Plot delle curve ROC
+    plt.figure(figsize=(10, 8))
+
+    plt.plot(fpr1, tpr1, color='darkorange', lw=2, label='Age(AUC = %0.2f)' % roc_auc1)
+    plt.plot(fpr2, tpr2, color='blue', lw=2, label='Avg Glucose Level(AUC = %0.2f)' % roc_auc2)
+    plt.plot(fpr3, tpr3, color='red', lw=2, label='BMI(AUC = %0.2f)' % roc_auc3)
+    plt.plot(fpr4, tpr4, color='green', lw=2, label='Categorical(AUC = %0.2f)' % roc_auc4)
+
+    # Linea di riferimento
+    plt.plot([0, 1], [0, 1], 'k--', lw=2)
+
+    # Impostazioni del grafico
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Confronto delle curve ROC - Decision Tree')
+    plt.legend(loc="lower right")
+
+    # Mostra il grafico
+    plt.show()
+
+def plot_roc_curve_conlusion_svm(y_pred_prod_Age, y_test_Age, y_pred_prod_glucose, y_test_glucose,
+                                  y_pred_prod_bmi, y_test_bmi, y_pred_prod_categorical, y_test_categorical):
+    # Calcolo della curva ROC per ogni modello
+    fpr1, tpr1, thresholds1 = roc_curve(y_test_Age, y_pred_prod_Age)
+    roc_auc1 = roc_auc_score(y_test_Age, y_pred_prod_Age)
+
+    # Calcolo della curva ROC per ogni modello
+    fpr2, tpr2, thresholds2 = roc_curve(y_test_glucose, y_pred_prod_glucose)
+    roc_auc2 = roc_auc_score(y_test_glucose, y_pred_prod_glucose)
+
+    # Calcolo della curva ROC per ogni modello
+    fpr3, tpr3, thresholds3 = roc_curve(y_test_bmi, y_pred_prod_bmi)
+    roc_auc3 = roc_auc_score(y_test_bmi, y_pred_prod_bmi)
+
+    # Calcolo della curva ROC per ogni modello
+    fpr4, tpr4, thresholds4 = roc_curve(y_test_categorical, y_pred_prod_categorical)
+    roc_auc4 = roc_auc_score(y_test_categorical, y_pred_prod_categorical)
+
+    # Plot delle curve ROC
+    plt.figure(figsize=(10, 8))
+
+    plt.plot(fpr1, tpr1, color='darkorange', lw=2, label='Age(AUC = %0.2f)' % roc_auc1)
+    plt.plot(fpr2, tpr2, color='blue', lw=2, label='Avg Glucose Level(AUC = %0.2f)' % roc_auc2)
+    plt.plot(fpr3, tpr3, color='red', lw=2, label='BMI(AUC = %0.2f)' % roc_auc3)
+    plt.plot(fpr4, tpr4, color='green', lw=2, label='Categorical(AUC = %0.2f)' % roc_auc4)
+
+    # Linea di riferimento
+    plt.plot([0, 1], [0, 1], 'k--', lw=2)
+
+    # Impostazioni del grafico
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Confronto delle curve ROC - Support Vector Machine')
+    plt.legend(loc="lower right")
+
+    # Mostra il grafico
+    plt.show()

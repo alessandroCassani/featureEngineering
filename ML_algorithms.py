fc8b6e21db516cc36e0eb25dda2cef120c2c980d
Luca Perfetti <l.perfetti2001@gmail.com>
2024-06-14 21:31:07 +0200

    new version

--- ML_algorithms.py
+++ ML_algorithms.py
@@ -13,75 +13,10 @@ from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn.svm import SVC
 from scipy.stats import reciprocal
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
-
-def train_decision_tree_model(df_dirty, df_original):
-    # Splitting the dataset con duplicati into features and target variable
-    X_dirty = df_dirty.drop('stroke', axis=1)
-    y_dirty = df_dirty['stroke']
-    
-    X_original = df_original.drop('stroke', axis=1)
-    y_original = df_original['stroke']
-
-    # Splitting the dirty dataset into training set and test set (with 30% testing)
-    X_train_dirty, X_test_dirty, y_train_dirty, y_test_dirty = train_test_split(X_dirty, y_dirty, test_size=0.3, random_state=42)
-    
-    X_train_original, X_test_original, y_train_original, y_test_original = train_test_split(X_original, y_original, test_size=0.3, random_state=42)
-
-    # Creating the decision tree classifier
-    tree_classifier = DecisionTreeClassifier(random_state=42)
-
-    # Defining the hyperparameter grid
-    param_dist = {
-        'criterion': ['gini', 'entropy'],
-        'max_features': ['sqrt', 'log2', None],
-        'min_samples_split': np.arange(2, 51, 2),
-        'min_samples_leaf': np.arange(1, 9),
-        'max_depth': [None, 5, 10, 15]
-    }
-
-    # Using RandomizedSearchCV for efficient hyperparameter search
-    random_search = RandomizedSearchCV(tree_classifier, param_distributions=param_dist, n_iter=100, cv=10, scoring='roc_auc', random_state=42)
-
-    # Measuring the start time for hyperparameter search
-    start_time_hyperparameter_search = time()
-    random_search.fit(X_train_dirty, y_train_dirty)
-    end_time_hyperparameter_search = time()
-    hyperparameter_search_time = end_time_hyperparameter_search - start_time_hyperparameter_search
-
-    # Getting the best parameters and best estimator
-    best_params = random_search.best_params_
-    best_tree_classifier = random_search.best_estimator_
-
-    # Training the model on the entire training set and measuring training time
-    start_time_training = time()
-    best_tree_classifier.fit(X_train_dirty, y_train_dirty)
-    end_time_training = time()
-    dt_training_time = end_time_training - start_time_training
-
-    # Predictions 
-    y_train_pred_dirty = best_tree_classifier.predict(X_train_dirty)
-    y_test_pred_original = best_tree_classifier.predict(X_test_original)
-
-    # Valutazione delle prestazioni sul set di addestramento
-    print("Classification Report on Training Set:")
-    print(classification_report(y_train_dirty, y_train_pred_dirty))
-
-    # Printing performance on the test set original
-    print("Classification Report on Test Set - original:")
-    print(classification_report(y_test_original, y_test_pred_original))
-
-    # Printing the best parameters and time taken for hyperparameter search and training
-    print("\nbest hyperparameter:", best_params)
-    print("hyperparameter's time search:", hyperparameter_search_time, "seconds")
-    print("training time needed:", dt_training_time, "seconds")
-    
-    plot_decision_tree(random_search.best_estimator_, feature_names=X_train_dirty.columns)
-    plot_feature_importance_decision_tree(best_tree_classifier, X_train_dirty)
-    plot_roc_curve(y_test_original, best_tree_classifier, X_test_original)
-    plot_confusion_matrix(y_test_original, y_test_pred_original)
-
-    return best_tree_classifier
 
 def plot_decision_tree(tree_model, feature_names, class_names=['0', '1']):
     plt.figure(figsize=(20, 10))
@@ -90,6 +25,7 @@ def plot_decision_tree(tree_model, feature_names, class_names=['0', '1']):
     text = tree_plot[0]
     print("Node Labels:\n", text)
     plt.show()
+
     
 def plot_feature_importance_decision_tree(best_tree_classifier, X):
     importance = best_tree_classifier.feature_importances_
@@ -166,10 +102,26 @@ def k_fold_cross_validation_dt(model, df):
     plt.show()
 
 def model_svm(df_dirty, df_original):
-    # Define parameter distributions for randomized search
-    param_distributions = {'C': reciprocal(0.1, 1000), 
-                           'gamma': reciprocal(0.001, 1),
-                           'kernel': ['rbf', 'linear']}
+    continuous_features = ['age', 'bmi', 'avg_glucose_level']
+    binary_features = ['sex', 'hypertension', 'heart_disease', 'ever_married', 'Residence_type', 'smoking_status']  
+    categorical_features = ['work_type']
+    
+    continuous_transformer = Pipeline(steps=[
+        ('scaler', StandardScaler())
+    ])
+
+    categorical_transformer = Pipeline(steps=[
+        ('onehot', OneHotEncoder())
+    ])
+    
+    
+    preprocessor = ColumnTransformer(
+    transformers=[
+        ('cont', continuous_transformer, continuous_features),
+        ('cat', categorical_transformer, categorical_features),
+        ('bin', 'passthrough', binary_features)
+    ])
+
     
     # Split the original dataset into features and target variable
     X_original = df_original.drop('stroke', axis=1)
@@ -183,30 +135,28 @@ def model_svm(df_dirty, df_original):
     
     # Initialize SVM model
     svm_model = SVC(kernel='linear', random_state=0)
-    svm_model.fit(X_train_dirty, y_train_dirty)
-
-    # Predict on the dirty test set
-    y_pred_dirty = svm_model.predict(X_test_dirty)
     
-    # Printing performance on the dirty test set
-    print("Classification Report on Dirty Test Set:")
-    print(classification_report(y_test_dirty, y_pred_dirty))
+    pipeline = Pipeline(steps=[
+        ('preprocessor', preprocessor),
+        ('classifier', svm_model)
+    ])
+    
+    pipeline.fit(X_train_dirty, y_train_dirty)
 
     # Predict on the original test set
-    y_pred_original = svm_model.predict(X_test_original)
+    y_pred_original = pipeline.predict(X_test_original)
     
     # Printing performance on the original test set
     print("Classification Report on Original Test Set:")
     print(classification_report(y_test_original, y_pred_original))
     
     # Plot ROC curve
-    plot_roc_curve_svm(y_test_original, svm_model, X_test_original)
-
+    plot_roc_curve_svm(y_test_original, pipeline, X_test_original)
     
     # Plot confusion matrix
     plot_confusion_matrix(y_test_original, y_pred_original)
     
-    return svm_model
+    return pipeline
 
 
     
@@ -225,12 +175,8 @@ def model_dt(df_dirty, df_original):
     
     decision_tree_model = DecisionTreeClassifier(max_depth=10, random_state=0)
     decision_tree_model.fit(X_train_dirty, y_train_dirty)
-    print("\n--- Prestazioni del modello Decision Tree applicato al set di Test: \n")
-    y_pred_dirty = decision_tree_model.predict(X_test_dirty)
     y_pred_original = decision_tree_model.predict(X_test_original)
     
-    print("Classification Report on Test Set - dirty:")
-    print(classification_report(y_test_dirty, y_pred_dirty))
 
     # Printing performance on the test set original
     print("Classification Report on Test Set - original:")
@@ -241,6 +187,7 @@ def model_dt(df_dirty, df_original):
     plot_roc_curve(y_test_original, decision_tree_model, X_test_original)
     plot_confusion_matrix(y_test_original, y_pred_original)
     return decision_tree_model
+
 
 def plot_roc_curve_svm(y_test, classifier, X_test):
     y_pred_prob = classifier.decision_function(X_test)
@@ -256,7 +203,7 @@ def plot_roc_curve_svm(y_test, classifier, X_test):
     plt.legend(loc="lower right")
     plt.show()
     print("AUC Score:", roc_auc)
-
+    
 def decision_tree(df):
     # Splitting the dataset con duplicati into features and target variable
     X = df.drop('stroke', axis=1)
@@ -285,12 +232,7 @@ def decision_tree(df):
     plot_confusion_matrix(y_test, pred_test)
     return decision_tree_model
 
-def SVM(df):
-    # Define parameter distributions for randomized search
-    param_distributions = {'C': reciprocal(0.1, 1000), 
-                           'gamma': reciprocal(0.001, 1),
-                           'kernel': ['rbf', 'linear']}
-    
+def SVM(df):    
     # Split the original dataset into features and target variable
     X = df.drop('stroke', axis=1)
     y = df['stroke']

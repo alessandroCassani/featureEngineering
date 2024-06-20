18febc30ea729d07c35f97e7d8ae17f5cbf4a000
alessandroCassani <acassani@studenti.uninsubria.it>
2024-06-21 01:15:32 +0200

    inaccuracy update

--- inaccuracy_dataset/util_inaccuracy.py
+++ inaccuracy_dataset/util_inaccuracy.py
@@ -1,5 +1,5 @@
-import random
 import pandas as pd
+import random
 
 def modify_values(column_name, dataset, percentage):
     # Ensure the dataset index is a RangeIndex (i.e., from 0 to len(dataset)-1)
@@ -8,7 +8,6 @@ def modify_values(column_name, dataset, percentage):
     
     num_rows = int(len(dataset) * (percentage / 100))
     rows_to_modify = random.sample(range(len(dataset)), num_rows)
-    original_dataset = dataset.copy()
 
     print(f"Modifying {num_rows} rows in column '{column_name}'")
     print(f"Rows to modify: {rows_to_modify}")
@@ -18,25 +17,16 @@ def modify_values(column_name, dataset, percentage):
             value = dataset.loc[i, column_name]
             if pd.notnull(value):
                 if value == 0:
-                    modified_value = value + 1  # Increment for 0
+                    modified_value = 1  # Increment for 0
                 elif value == 4:
-                    modified_value = value - 1  # Decrement for 4
-                elif 1 <= value <= 3:
+                    modified_value = 3  # Decrement for 4
+                else:
                     modified_value = value + random.choice([-1, 1])  # Increment or decrement for 1, 2, 3
                 dataset.loc[i, column_name] = modified_value
-
-    elif pd.api.types.is_integer_dtype(dataset[column_name]):
-        for i in rows_to_modify:
-            value = dataset.loc[i, column_name]
-            if pd.notnull(value) and value != 0 and value != 1:  # Skip binary features
-                modified_value = int(value * 1.3)  # Increase the value by 10%
-                dataset.loc[i, column_name] = modified_value
-
     elif pd.api.types.is_float_dtype(dataset[column_name]):
         for i in rows_to_modify:
             value = dataset.loc[i, column_name]
-            if pd.notnull(value) and value != 0.0 and value != 1.0:  # Skip binary features
-                modified_value = value * 1.3  # Increase the value by 10%
-                dataset.loc[i, column_name] = modified_value
+            modified_value = value * 1.3  # Increase the value by 30%
+            dataset.loc[i, column_name] = modified_value
 
-    return dataset  
+    return dataset
